5622b755cc54e54e7d559b76b5666253b5afd917
Alejandro Daniel Di Stefano <addistefano76@gmail.com>
2026-08-23 18:35:59 -0300

    deploy: sync gh-pages with main v2.1.0

--- assets/js/models/Alumno.js
+++
@@ -1,2 +0,0 @@
-import { Persona } from './Persona.js';
-export class Alumno extends Persona{matricula;cursos;constructor(id,nombre,apellido,nacimiento,dni,matricula,email){super(id,nombre,apellido,nacimiento,dni,'Alumno',email);this.matricula=matricula;this.cursos=[]}inscribirse(comisiones){this.cursos=comisiones}mostrarDatos(){return `Alumno: ${this.nombre} ${this.apellido}, Matrícula: ${this.matricula}, Edad: ${this.calcularEdad()}`}}
--- assets/js/models/Categoria.js
+++
@@ -1,1 +0,0 @@
-export class Categoria{id;categoria;constructor(id,categoria){this.id=id;this.categoria=categoria}mostrarDetalles(){return `Categoria: ${this.categoria} Agregada con éxito.!`}}
